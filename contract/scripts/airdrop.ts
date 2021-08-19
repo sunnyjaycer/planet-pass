aa9c992a7ed141df0c6bc39dda85a27887e8f1dd
Anson <git@anson-cheung.com>
2021-08-20 03:52:33 +0800

    Add airdrop script

--- contract/scripts/airdrop.ts
+++ contract/scripts/airdrop.ts
@@ -1,5 +1,15 @@
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
+import { id } from "ethers/lib/utils";
 import { INFURA_PROJECT_ID, INFURA_PROJECT_SECRET } from "../secrets";
+import "fs";
+import { writeFileSync } from "fs";
+
+const WANDERER_ADDRESS = "0x8184a482A5038B124d933B779E0Ea6e0fb72F54E";
+
+const DEPLOYED_BLOCK = 13013300;   // make sure we start at block divisible by 10
+const END_BLOCK = 13056640;
+const SKIP_SIZE = 1000;
+
 
 // Refer to https://ethereum.stackexchange.com/questions/61565/list-holders-and-tokens-for-an-erc-721-contract
 
@@ -9,8 +19,40 @@ async function main() {
         projectSecret: INFURA_PROJECT_SECRET
     });
     let owners: Map<number, string> = new Map();
-    console.log(owners.get(0));
-    console.log(ethers.utils.formatEther(await provider.getBalance("esmeralda.eth")));
+
+    console.log("Fetching logs");
+
+    let x = 0;
+    for (let i = 0; i < (END_BLOCK - DEPLOYED_BLOCK) / SKIP_SIZE; i++) {
+        const fromBlock = DEPLOYED_BLOCK + (i * SKIP_SIZE);
+        let toBlock = fromBlock + SKIP_SIZE;
+        if (toBlock > END_BLOCK) {
+            toBlock = END_BLOCK;
+        }
+
+        console.log(fromBlock, toBlock, toBlock - fromBlock);
+        const filter = {
+            address: WANDERER_ADDRESS,
+            topics: [
+                id("Transfer(address,address,uint256)")
+            ],
+            fromBlock: fromBlock,
+            toBlock: toBlock,
+        }
+        const logs = await provider.getLogs(filter);
+        for (let log of logs) {
+            const n = BigNumber.from(log.topics[3]);
+            let a = ethers.utils.hexlify(log.topics[2]);
+            a = ethers.utils.hexDataSlice(a, 12);
+            owners.set(n.toNumber(), ethers.utils.getAddress(a).toString());
+        }
+
+        x += logs.length;
+    }
+
+    console.log("Fetched", x, "transfers");
+    const json = JSON.stringify(Object.fromEntries(owners));
+    writeFileSync("output.json", json);
 }
 
 main()
