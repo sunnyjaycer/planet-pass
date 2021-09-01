d8faf837fad9fdcea5a3f8463718c25b7136d594
Anson <git@anson-cheung.com>
2021-09-02 04:18:54 +0800

    Add contract owner fee

--- contract/contracts/Flashstamp.sol
+++ contract/contracts/Flashstamp.sol
@@ -7,11 +7,20 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Flashstamp is IERC721Receiver {
+contract Flashstamp is IERC721Receiver, Ownable {
+    // Basis point: part per 10,000.
+    uint256 public constant BASIS_POINTS_DIVISOR = 10000;
+
     WanderersPlanet public planetContract;
     WanderersPass public passContract;
     IERC20 public wEthContract =
         IERC20(0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619);
+
+    // Fee charged by the contract deployer in basis points
+    uint256 public ownerFeeBp;
+
+    // Fee accured by contract owner
+    uint256 public ownerFeesAccrued;
 
     // Mapping of Planet IDs to owners.
     mapping(uint256 => address) planetOwners;
@@ -20,7 +29,11 @@ contract Flashstamp is IERC721Receiver {
     mapping(uint256 => uint256) planetFees;
 
     // Mapping of owners to fees accured
-    mapping(address => uint256) feesAccured;
+    mapping(address => uint256) feesAccrued;
+
+    constructor(uint256 _ownerFeeBp) {
+        ownerFeeBp = _ownerFeeBp;
+    }
 
     // Perform a flash-stamp of planetId onto passId.
     // Note: this requires an approval from Planet Pass for passId, otherwise it will revert.
@@ -28,14 +41,22 @@ contract Flashstamp is IERC721Receiver {
     function flashStamp(uint256 planetId, uint256 passId) public {
         require(planetOwners[planetId] != address(0), "Planet not in contract");
 
-        // Send WETH to contract
         uint256 fee = planetFees[planetId];
+        address planetOwner = planetOwners[planetId];
+
+        // Send WETH to contract
         if (fee != 0) {
-            feesAccured[planetOwners[planetId]] += fee;
+            // Calculate contract deployer's cut and planet owner's cut
+            uint256 ownerFee = calculateOwnerFee(fee);
+            uint256 payoutFee = fee - ownerFee;
+
+            feesAccrued[planetOwner] += payoutFee;
+            ownerFeesAccrued += ownerFee;
+
             bool success = wEthContract.transferFrom(
                 msg.sender,
                 address(this),
-                planetFees[planetId]
+                payoutFee
             );
             require(success, "Token transfer failed");
         }
@@ -50,9 +71,25 @@ contract Flashstamp is IERC721Receiver {
         passContract.safeTransferFrom(address(this), msg.sender, passId);
     }
 
+    function calculateOwnerFee(uint256 fee) internal view returns (uint256) {
+        return (fee * ownerFeeBp) / BASIS_POINTS_DIVISOR;
+    }
+
+    function withdrawownerFeesAccrued(address to) public onlyOwner {
+        uint256 transferAmount = ownerFeesAccrued;
+        ownerFeesAccrued = 0;
+
+        bool success = wEthContract.transferFrom(
+            address(this),
+            to,
+            transferAmount
+        );
+        require(success, "Token transfer failed");
+    }
+
     function withdrawFees(address to) public {
-        uint256 transferAmount = feesAccured[msg.sender];
-        feesAccured[msg.sender] = 0;
+        uint256 transferAmount = feesAccrued[msg.sender];
+        feesAccrued[msg.sender] = 0;
 
         bool success = wEthContract.transferFrom(
             address(this),
