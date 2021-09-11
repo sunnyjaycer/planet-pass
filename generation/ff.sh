90d9c0d6b440198eb8ea867a559babc2b63ca632
Anson <git@anson-cheung.com>
2021-09-12 04:46:32 +0800

    Optimise video file-sizes

--- generation/ff.sh
+++ generation/ff.sh
@@ -7,17 +7,17 @@ for i in {0..8887}; do
   count=$(find ./*.wav 2>/dev/null | wc -l)
   if [ "$count" -gt 1 ]; then
     # wav files found
-    sox -M -c 2 ./*.wav output.wav
-    ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -i output.wav -c:a aac -c:v libx264 -crf 12 -pix_fmt yuv420p -profile:v main ../../videos/"${i}".mp4
+    sox -m ./*.wav output.wav
+    ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -i output.wav -c:a aac -c:v libx264 -crf 18 -pix_fmt yuv420p -profile:v high -level 4.2 ../../videos/"${i}".mp4
     # clean up so if we re-run it doesn't get mixed again
     rm output.wav
   elif [ "$count" == 1 ]; then
     # only 1 wav file found, sox can't combine
     name=$(find ./*.wav)
-    ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -i "$name" -c:a aac -c:v libx264 -crf 12 -pix_fmt yuv420p -profile:v main ../../videos/"${i}".mp4
+    ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -i "$name" -c:a aac -c:v libx264 -crf 18 -pix_fmt yuv420p -profile:v high -level 4.2 ../../videos/"${i}".mp4
   else
     # no wav files found
-    ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -c:v libx264 -crf 12 -pix_fmt yuv420p -profile:v main ../../videos/"${i}".mp4
+    ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -c:v libx264 -crf 18 -pix_fmt yuv420p -profile:v high -level 4.2 ../../videos/"${i}".mp4
   fi
   cd ..
 done
