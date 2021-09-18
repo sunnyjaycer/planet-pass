f8ead29fd11b13c08faabd5b8d5dc6b0b732b64e
Anson <git@anson-cheung.com>
2021-09-18 23:53:10 +0800

    Add template Stardust contract

---
+++ contract/test/test-stardust.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { ethers } from "hardhat";
+import { BigNumber, Signer } from "ethers";
+import { expect, use } from "chai";
+import { solidity } from "ethereum-waffle";
+import { parseEther, solidityPack } from "ethers/lib/utils";
+
+use(solidity);
+
+describe("Stardust", function () {
+    
+})
