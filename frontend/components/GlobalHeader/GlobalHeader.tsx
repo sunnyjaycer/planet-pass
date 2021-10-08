d6b831aae7f7e112820b6b5883834a10bfcc765d
Chris Arasin <chris.arasin@gmail.com>
2021-10-08 13:25:54 -0400

    claim ui

--- frontend/components/GlobalHeader/GlobalHeader.tsx
+++ frontend/components/GlobalHeader/GlobalHeader.tsx
@@ -1,6 +1,7 @@
 import style from './GlobalHeader.module.scss'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import Button from '../Button'
 
 const navLinks = [
   {
@@ -26,31 +27,48 @@ const GlobalHeader = () => {
 
   return (
     <header className={style.globalHeader}>
-      <Link href="/">
-        <a className={style.headerLogo}>Wanderers</a>
-      </Link>
-      <nav className={style.globalNav}>
-        <ul>
-          {navLinks.map((linkItem, i) => (
-            <li key={linkItem.url + linkItem.name + i}>
-              <Link href={linkItem.url}>
-                <a
-                  className={
-                    (linkItem.url === '/' &&
-                      router.pathname === linkItem.url) ||
-                    (linkItem.url !== '/' &&
-                      router.pathname.includes(linkItem.url))
-                      ? style.activeNav
-                      : ''
-                  }
-                >
-                  {linkItem.name}
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <div className={style.headerMain}>
+        <Link href="/">
+          <a className={style.headerLogo}>Wanderers</a>
+        </Link>
+        <nav className={style.globalNav}>
+          <ul>
+            {navLinks.map((linkItem, i) => (
+              <li key={linkItem.url + linkItem.name + i}>
+                <Link href={linkItem.url}>
+                  <a
+                    className={
+                      (linkItem.url === '/' &&
+                        router.pathname === linkItem.url) ||
+                      (linkItem.url !== '/' &&
+                        router.pathname.includes(linkItem.url))
+                        ? style.activeNav
+                        : ''
+                    }
+                  >
+                    {linkItem.name}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <div className={style.earningsWrap}>
+        <div className={style.earnings}>
+          <span className={style.earningsLabel}>Earnings </span>
+          <span className={style.earningsAmount}>13.4 WETH</span>
+          <div className={style.earningsClaim}>
+            <Button
+              text="Claim"
+              buttonStyle="secondary"
+              handleClick={() => {
+                alert('TODO: Handle Claim')
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
