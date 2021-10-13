b5f123c393298486d590023c599a6cd775173cad
Anson <git@anson-cheung.com>
2021-10-14 06:46:33 +0800

    Remove b2 scripts

--- generation/b2/metadata.sh
+++
@@ -1,5 +0,0 @@
-cd /mnt/e/planetpass/metadata2/$2 || exit
-
-for file in *; do
-    b2 upload_file --threads 24 --contentType application/json $1 $file metadata/$2/$file
-done
--- generation/b2/videos.sh
+++
@@ -1,5 +0,0 @@
-cd /mnt/e/planetpass/videos || exit
-
-for file in *; do
-    b2 upload_file --threads 24 $1 $file video/$2/$file
-done
